b3b534d6c0c3424283f5b0d0bf716e8bec44b140
Daniel <djs66256@163.com>
2017-09-09 01:41:42 +0800

    update

--- deploy.sh
+++ deploy.sh
@@ -1,14 +1,20 @@
 #! /bin/sh
 
+echo '=========================='
 echo 'Begin deploy hexo blog ...'
 hexo deploy -g
 echo 'Finish deploy hexo blog.'
+echo '=========================='
+
+echo ''
 
 # push hexo
+echo '=========================='
 echo 'Begin push to hexo ...'
 git add *
 git commit -m 'update'
 git pull origin hexo
 git push origin hexo
 echo 'Finish push to hexo.'
+echo '=========================='
 
