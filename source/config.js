105f3b626a1743a6fd8a106937c1efe49022e06d
Daniel <djs66256@163.com>
2017-09-30 21:48:56 +0800

    amend

--- source/config.js
+++ source/config.js
@@ -3,5 +3,6 @@ module.exports = {
   projectPath: path.resolve(__dirname, '_posts'),
   projectName: 'project.md',
   dataPath: path.resolve(__dirname, '.repos'),
-  usersDataName: 'users.json'
+  usersDataName: 'users.json',
+  destinationPath: __dirname
 }
