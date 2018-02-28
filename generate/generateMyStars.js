757689830451464c23d583d26a720d2578f11517
Daniel <djs66256@163.com>
2018-03-01 00:00:27 +0800

    update

--- generate/generateMyStars.js
+++ generate/generateMyStars.js
@@ -5,12 +5,12 @@ const { Generator } = require('./generator')
 let username = 'djs66256'
 
 let user = null
-if (userConfigs.getUserConfigWithName(username)) {
-  user = User.build({ type: 'cache', name: username })
-}
-else {
+// if (userConfigs.getUserConfigWithName(username)) {
+//   user = User.build({ type: 'cache', name: username })
+// }
+// else {
   user = User.build({ type: 'github', name: username })
-}
+// }
 
 Generator.build({ user }).generateStarRepos().catch(e => {
   console.error(`[ERROR] ${e.message}`)
---
+++ source/_posts/project2.md
@@ -0,0 +1,19 @@
+---
+title: 开源项目列表-C++
+date: 2012-07-17 01:57:36
+categories:
+- 开源项目
+tags:
+---
+
+收藏的开源项目以及部分分析。C++相关部分。
+
+<!--more-->
+
+- [protobuf](https://github.com/google/protobuf) **20416 Stars** **C++** Protocol Buffers - Google's data interchange format
+
+以一种流的形式编码数据，来达到更高的压缩率和更高的解码效率。通过`.proto`文件，严格约束各端的数据结构，并且生成代码，减少了各端的实现成本。其中也包含了rpc相关的实现。
+
+- [grpc](https://github.com/grpc/grpc) **13827 Stars** **C++** The C based gRPC (C++, Python, Ruby, Objective-C, PHP, C#)
+
+利用protobuf实现的rpc通信，简单且通用。
